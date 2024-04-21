d043308bc9338f14a9f42eacc6fa7ca82db0bdd1
AizeN1337 <maciejkalata79@gmail.com>
2024-04-21 12:53:36 +0200

    landing page and --turbo

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: "Dashboard",
+  description: "Banking app dashboard",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,89 @@
-import Link from "next/link";
-
+import Link from 'next/link';
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      aurum bank
-    </main>
+    const navigation = [
+      { name: 'Product', href: '#' },
+      { name: 'Features', href: '#' },
+      { name: 'Marketplace', href: '#' },
+      { name: 'Company', href: '#' },
+    ]
+    
+      return (
+        <div className="bg-zinc-900 h-screen">
+          <header className="absolute inset-x-0 top-0 z-50">
+            <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+              <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                </a>
+              </div>
+              <div className="flex lg:hidden">
+                <button
+                  type="button"
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-100"
+                >
+                  <span className="sr-only">Open main menu</span>
+                </button>
+              </div>
+              <div className="hidden lg:flex lg:gap-x-12">
+                {navigation.map((item) => (
+                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-100">
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-100">
+                  Log in <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </nav>
+
+          </header>
+    
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#886442] to-[#cf0202] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                }}
+              />
+            </div>
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-100 ring-1 ring-gray-100/10 hover:ring-gray-100/20">
+                  Announcing our next round of funding.{' '}
+                  <a href="#" className="font-semibold text-amber-600">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    Read more <span aria-hidden="true">&rarr;</span>
+                  </a>
+                </div>
+              </div>
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">
+                  Modern banking for you.
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-100">
+                Feel free to explore our finance solutions, tailor made just for you.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <Link
+                    href="#"
+                    className="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
+                  >
+                    Get started
+                  </Link>
+                  <Link href="#" className="text-sm font-semibold leading-6 text-gray-100">
+                    Learn more <span aria-hidden="true">→</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
   );
 }
